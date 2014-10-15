6f59d89929422c7aa106d228225cc19410c7ea7a
adityamangipudi <adityamangipudi@users.noreply.github.com>
2014-10-15 13:02:02 -0700

    x

--- string_reduction.py
+++ string_reduction.py
@@ -1,59 +1,45 @@
 #!/usr/bin/py
 # Head ends here
+string_memo = {}
+def stringMemo(a):
+    if not a in string_memo:
+        string_memo[a]= stringReduction(a)
+    
+    for k, v in string_memo.items():
+        print ("sup \n" )
+        print (k, v)
+    return string_memo[a]
 
-#checks the two letter value of the string and reduces using to one letter
-def str_reduce(st):
-    if st=='ab'or st =='ba' :
-        return 'c'
-    if st =='ca' or st =='ac' :
-        return 'b'
-    if st =='cb' or st =='bc' :
-        return 'a'
-    return st
 
 
+#uses memoization to keep a list of reduced strings.
 def stringReduction(a):
-    answer =0 
-    strlength=len(a)
-    #length is 1
-    if strlength ==1 and (a[0] =='a' or a[0] =='b' or a[0] =='c'):
-        answer =1
-    #length is 2
-    if strlength ==2:
-        #if it can be reduced length is 1
-        twolenstr = str_reduce(a)
-        #set length to length of new string
-        answer =len(twolenstr)
-    #if length is larger than 2 then we parse the string and modify accordingly
-    if strlength>2:
-        answer = len(a)
-        #for loop for all values in the string
-        for i in range(len(a)-1):
+    answer =len(a)
+    #put the string in a list
+    strlist = [a]
+    #while theres a string in the list go into loop
+    while strlist: 
+        #pop the string at the end of the list
+        strval = strlist.pop()
+        #check if strval is smaller than stored length before trying to further reduce it 
+        if answer>len(strval):
+            answer=len(strval)
+        for i in range(len(strval)-1):
             #loop into if the character is not same as the character after it
-            if a[i] !=a[i+1]:
-                #substring of the 2 letters that aren't same and reduce it
-                substr = a[i:i+2]
-                new_string_st1=''
-                new_string_st2=''
-                reduced_str = str_reduce(substr)
-                
-                #create prefix of the new string to be appended
-                if i==1:
-                    new_string_st1= a[0]
-                elif i>1:
-                    new_string_st1=a[0:i]
-                #create suffix of the new string to be appended
-                if (i+2)<(len(a)-1):
-                    new_string_st2= a[i+2:]
-                elif (i+2)==(len(a)-1):
-                    new_string_st2=a[i+2]
-                #new string stord
-                new_string = new_string_st1+reduced_str+new_string_st2
-                #calculate min of new string recursively
-                new_min = stringReduction(new_string)
-                # if min of new string is smaller, set the answer to new min value
-                if new_min<answer:
-                    answer=new_min
+            if strval[i] !=strval[i+1]:
+                #take the substring of the values that are not equal 
+                #from the shortened string popped off the end of list
+                substr = strval[i:i+2]
+                #check the values of the substring and edit them accordingly
+                if substr=='ab'or substr =='ba' :
+                    substr = 'c'
+                if substr =='ca' or substr =='ac' :
+                    substr = 'b'
+                if substr =='cb' or substr =='bc' :
+                    substr = 'a'
+                #add the new shortened string back into the front of the 
+                #list to see if it can be shortened further
+                strlist.insert(0,strval[:i]+substr+strval[(i+2):])
     #return value of length
     return answer
 
@@ -63,6 +49,7 @@ if __name__ == '__main__':
     result=''
     for i in range(0,t):
         a=input()
-        result +=str(stringReduction(a))+'\n'
+        result +=str(stringMemo(a))+'\n'
+    
     
     print(result.rstrip('\n'))
